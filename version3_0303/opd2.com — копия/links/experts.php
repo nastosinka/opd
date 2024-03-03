3269fabbd69a59fb7431f1b37591a2f25c0689c3
Iara Shilova <161764916+Slavaoutofcontrol@users.noreply.github.com>
2024-03-03 20:33:24 +0300

    Update experts.php

--- version3_0303/opd2.com — копия/links/experts.php
+++ version3_0303/opd2.com — копия/links/experts.php
@@ -1,3 +1,17 @@
+<?php
+
+  $db = new PDO("mysql:host=localhost;dbname=opd",
+  "root", "");
+
+  $info = [];
+
+  if ($query = $db->query("Select * from user where flag='e'")) {
+    $info = $query->fetchAll(PDO::FETCH_ASSOC);} else {
+        print_r($db->errorInfo());
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -5,37 +19,29 @@
     <link rel="stylesheet" href="../assets/css/experts.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'> <title>Document</title>
 </head>
-<?php require_once("connection.php"); ?>
 <body>
-   <?php
-   echo "<div class='cards'>";
-   $result = $con->query('SELECT * FROM user WHERE flag="e"'); // запрос на выборку
-while($row = $result->fetch_assoc())// получаем все строки в цикле по одной
-{
-  if ($row["photo"] == null){
-    $photo = '../assets/images/expert2.jpg';
-  } else{
-    $photo = $row['photo'];
-  }
-    echo "<div class='card'>
-    <div class='card__top'>
-    <div class='card__image'>
-      <img src='".$photo."'";
-        echo "alt='Фото эксперта'
-      />
+    <div class="cards">
+    <?php foreach ($info as $data): ?>
+  <div class="card">
+    <div class="card__top">
+    <div class="card__image">
+      <?php if ($data['photo'] == null){
+              $photo = '../assets/images/expert2.jpg';
+            } else{
+              $photo = $data['photo'];
+            }?>
+      <img src=<?php echo $photo; ?>
+        alt="Фото эксперта">/>
     </div>
-    <div class='image__text'>
-    <h1 class='card__title'>";
-    echo $row['name'];
-    echo "</h1>
-    <h2>Достижения</h2>
-    <p>";
-    echo $row['achievements'];
-    echo "</p>
-</div>
-</div>";
-   }
-  ?>
-</div>
+    <div class="image__text">
+        <h1><?php echo $data['name']; ?></h1>
+        <h2><?php echo 'Достижения'; ?></h2>
+        <p><?php echo $data['achievements']; ?></p>
+    </div>
+  </div>
+  </div>
+  <?php endforeach; ?>
+  </div>
+  </div>
 </body>
 </html>
